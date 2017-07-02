0d97eb4ab8fb29fc1f2360a35c86fdcf8bff60fa
Cyrill <Cyrill@DESKTOP-LD03T4T>
2017-07-02 19:43:51 +0200

    Loading saves on enter of Menustate

--- 2DSpeedEngine/src/gui/Menu.java
+++ 2DSpeedEngine/src/gui/Menu.java
@@ -1,5 +1,13 @@
 package gui;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -8,13 +16,20 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import game.HighScore;
 
 public class Menu extends BasicGameState
 {
-	int myIndex;
-	int mPosX = 0;
-	int mPosY = 0;
-	String buttonClicked = "none";
+	private int myIndex;
+	private int mPosX = 0;
+	private int mPosY = 0;
+	private String buttonClicked = "none";
+	private ArrayList<HighScore> highscore;
 	
 	public Menu(int index)
 	{
@@ -23,9 +38,15 @@ public class Menu extends BasicGameState
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException
 	{
-		
+		highscore = new ArrayList<HighScore>();
+		readXMLsaves("save/highscore.xml");
 	}
-
+	
+	public void enter(GameContainer gc, StateBasedGame sbg)
+	{
+		readXMLsaves("save/highscore.xml");
+	}
+	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException
 	{
 		g.setBackground(Run.backgroundColor);
@@ -63,6 +84,42 @@ public class Menu extends BasicGameState
 		return myIndex;
 	}
 
+	public void readXMLsaves(String filename)
+	{
+		Document dom;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		try
+		{
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			dom = db.parse(filename);
+			
+			Element doc = dom.getDocumentElement();
+			NodeList scoresFromXML = doc.getChildNodes();
+			
+			// save every xml score tag in highscore ArrayList
+			for (int i = 0; i<scoresFromXML.getLength(); i++)
+			{
+				NodeList thisscore = scoresFromXML.item(i).getChildNodes();
+				long thisTime = Long.parseLong(thisscore.item(0).getTextContent());
+				String thisName = thisscore.item(1).getTextContent();
+				long thisTimeMillis = Long.parseLong(thisscore.item(2).getTextContent());
+				highscore.add(new HighScore(thisTime, thisName, new Date(thisTimeMillis)));
+			}
+		}
+		catch (ParserConfigurationException pce) 
+		{
+            System.out.println(pce.getMessage());
+        } 
+		catch (SAXException se) 
+		{
+            System.out.println(se.getMessage());
+        } 
+		catch (IOException ioe) 
+		{
+            System.err.println(ioe.getMessage());
+        }
+	}
+	
 	//overwrite mouseReleased method for button click handling
 	public void mouseReleased(int button, int x, int y)
 	{
